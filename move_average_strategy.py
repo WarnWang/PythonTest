83bebc0e2a326f876af5e276bf5f2ac5c16e5c82
WarnMark <wangyouan0629@hotmail.com>
2015-12-26 17:31:51 +0800

    add get price from internet

--- move_average_strategy.py
+++ move_average_strategy.py
@@ -5,34 +5,54 @@
 # Author: warn
 # Date: 25/12/2015 20:49
 
+import urllib
+import urllib2
+
+import numpy
+import talib
 
 import cashAlgoAPI
-import talib
-import numpy
 
 
 class Strategy:
     # self.mgr: order manager
     # self.config: configuration reader from parameter panel
 
+    # used to get data from finance yahoo
+    DATA_URL = "http://real-chart.finance.yahoo.com/table.csv"
+
     def __init__(self):
 
         # how many orders execute
         self.cnt = 0
 
-        # The daily open price of each product
+        # Remember the last price of last trading
         self.last_price = None
+
+        # Used to remark whether this market data is an open date or not.
         self.last_date = None
+
+        # how many volume stocks we have bought
         self.buy_volume = 0
+
+        # the days used to calculate the move average
         self.move_average_day = 0
+
+        # the close price of last days
         self.close_price = None
-        # self.end_date = None
+
+        # usually 2, the confidential factor.
         self.factor = None
+
+        # How many total capital we have in summary
         self.total_capital = None
+
+        # How many capital we have
         self.current_capital = None
 
     # Initialize Strategy
     def init(self):
+
         # Read Parameters
         if self.config.has_option("Strategy", "MeanAverage"):
             self.move_average_day = int(self.config.get("Strategy", "MeanAverage"))
@@ -40,21 +60,27 @@ class Strategy:
         # self.end_date = self.config.get("MarketData", "EndDate").replace("-", "")
         self.factor = float(self.config.get("Strategy", "Factor"))
         self.current_capital = self.total_capital = float(self.config.get("Risk", "InitialCapital"))
+        product_code = self.config.get("MarketData", "ProductCode_1")
 
         self.cnt = 0
         self.last_price = None
         self.last_date = None
         self.buy_volume = 0
-        self.close_price = [float(i) for i in self.config.get("Strategy", "ClosePrice").split(',')]
+
+        # Get the past price of this stock.
+        self.close_price = self.get_price_data(product_code)
+        # self.close_price = [float(i) for i in self.config.get("Strategy", "ClosePrice").split(',')]
 
     # Process Market Data. Please use onOHLCFeed() in OHLC mode
     def onMarketDataUpdate(self, market, code, md):
 
+        # The following time is not allowed to trade. Only trade from 9:30 am to 12:00 am, and from 13:00 to 16:00
         time_info = md.timestamp.split('_')
-        if int(time_info[1]) not in (range(93000, 120000) + range(130000, 160000)):
+        if int(time_info[1][:2]) not in (range(10, 12) + range(13, 16)) or (time_info[1][:2] == '09' and
+                                                                            int(time_info[1][2:]) < 3000):
             return
 
-        # For open price
+        # For open price record last day close price
         if time_info[0] != self.last_date:
             self.last_date = time_info[0]
 
@@ -65,11 +91,13 @@ class Strategy:
         mean_average = talib.MA(numpy.array(self.close_price), self.move_average_day)[-1]
         standard_dev = talib.STDDEV(numpy.array(self.close_price))[-1]
 
-        if md.lastPrice < 1:
+        # in case some bad value
+        if md.lastPrice < mean_average / 10:
             md.lastPrice *= 100
+
         if md.lastPrice + self.factor * standard_dev < mean_average:
             volume = int(self.total_capital / 10 / md.lastPrice)
-            if self.current_capital > self.total_capital / 10:
+            if self.current_capital > volume * md.lastPrice:
                 order = cashAlgoAPI.Order(md.timestamp, 'SEHK', code, str(self.cnt), md.askPrice1, volume,
                                           "open", 1, "insert", "market_order", "today")
 
@@ -124,3 +152,33 @@ class Strategy:
         # print "dailyPnL: %s" % pf.dailyPnL
         pass
 
+    @staticmethod
+    def get(url, data=None):
+        if data is not None:
+            url_values = urllib.urlencode(data)
+            url = "%s?%s" % (url, url_values)
+        req = urllib2.Request(url)
+        response = urllib2.urlopen(req)
+        content = response.read()
+        return content
+
+    def get_price_data(self, code, start_date="2014-08-28", end_date="2015-01-01"):
+        code = '%s.HK' % code[1:]
+        time_info = [("s", code)]
+        data = start_date.split('-')
+        time_info.append(("a", "%02d" % (int(data[1])  - 1)))
+        time_info.append(("b", data[2]))
+        time_info.append(("c", data[0]))
+
+        data = end_date.split('-')
+        time_info.append(("d", "%02d" % (int(data[1]) - 1)))
+        time_info.append(("e", data[2]))
+        time_info.append(("f", data[0]))
+
+        time_info.append(("g", "d"))
+        time_info.append(("ignore", ".csv"))
+
+        price_info = self.get(self.DATA_URL, time_info).split('\n')
+        price_list = [float(i.split(',')[4]) for i in price_info]
+        price_list = reversed(price_list)
+        return price_list
