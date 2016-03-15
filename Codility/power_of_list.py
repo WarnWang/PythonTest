fc72b9b0a262ef899c39a804e4dd97fefb4f4d92
WarnMark <wangyouan0629@hotmail.com>
2016-03-15 10:09:12 +0800

    add some information about leetcode algorithm

--- Codility/power_of_list.py
+++ Codility/power_of_list.py
@@ -17,5 +17,40 @@ def find_power(nums):
     return result
 
 
+class Solution(object):
+    def zigzagLevelOrder(self, root):
+        node_list = [root]
+        current_level_node_num = 0
+        current_level_max_num = 1
+        zigzag = [[]]
+        switch_order = True
+        while node_list:
+            if current_level_node_num == current_level_max_num:
+                if current_level_max_num == 0:
+                    break
+                zigzag.append([])
+                current_level_max_num *= 2
+                current_level_node_num = 0
+                switch_order = not switch_order;
+            node = node_list.pop(0)
+
+            if node is not None:
+                zigzag[-1].append(node.val)
+                current_level_node_num += 1
+                if switch_order:
+                    node_list.append(node.left)
+                    node_list.append(node.right)
+                else:
+                    node_list.append(node.right)
+                    node_list.append(node.left)
+
+            else:
+                current_level_max_num -= 1
+
+        if not zigzag[-1]:
+            zigzag.pop()
+        return zigzag
+
+
 if __name__ == "__main__":
     print find_power([1, 2, 3])
