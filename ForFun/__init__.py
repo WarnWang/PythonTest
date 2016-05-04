69129e68e08995e3712d398d734fc0b8e84cfd08
Mark Wang <wangyouan0629@hotmail.com>
2016-05-04 10:03:51 +0800

    Add file for testing leetcode

---
+++ ForFun/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Project: PythonTest
+# File name: __init__.py
+# Author: Mark Wang
+# Date: 4/5/2016
---
+++ ForFun/generate_list.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Project: PythonTest
+# File name: generate_list
+# Author: Mark Wang
+# Date: 4/5/2016
+
+
+import random
+
+
+def get_large_list(length=100, minimum=1, maximum=10):
+    large_list = []
+    for i in range(length):
+        large_list.append(random.randrange(start=minimum, stop=maximum))
+
+    return large_list
+
+
+if __name__ == '__main__':
+    print get_large_list()
