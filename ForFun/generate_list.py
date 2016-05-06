b3582d9bd6a9c4f4a09cde4a088c7a6610ca834c
Mark Wang <wangyouan0629@hotmail.com>
2016-05-06 19:07:15 +0800

    Add new method about generate matrix

--- ForFun/generate_list.py
+++ ForFun/generate_list.py
@@ -9,6 +9,8 @@
 
 import random
 
+import numpy.random as np_random
+
 
 def get_large_list(length=100, minimum=1, maximum=10):
     large_list = []
@@ -18,5 +20,13 @@ def get_large_list(length=100, minimum=1, maximum=10):
     return large_list
 
 
+def generate_matrix(width, height, minimum=1, maximum=10):
+    return np_random.randint(low=minimum, high=maximum, size=[width, height], dtype=int)
+
+
 if __name__ == '__main__':
-    print get_large_list(length=25000)
+    # print get_large_list(length=25000)
+    a = generate_matrix(100, 100, 1, 20)
+    print a.tolist()
+    # import pprint
+    # pprint.pprint(a.tolist(), width=500)
